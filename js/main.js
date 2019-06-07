a54bd06bdd193ec41564162e8fde58e755497f7a
Aray Karjauv <kararray@gmail.com>
2019-06-07 22:09:06 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// $(function () {
+//     var header = $("#sticky");
+//     var parallax = $('.parallax');
+
+//     parallax.on('scroll', function () {
+//         if (parallax.scrollTop >= header.scrollTop) {
+//             header.addClass("sticky");
+//         } else {
+//             header.removeClass("sticky");
+//         }
+//     });
+// })
