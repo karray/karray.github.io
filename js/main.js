a309d71447e418589af4242aa7942354991ebb49
Aray <s0562411@htw-berlin.de>
2019-08-14 23:11:45 +0200

    ''

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-// $(function () {
-//     var header = $("#sticky");
-//     var parallax = $('.parallax');
+$(function () {
+    var sticky = $("#sticky");
+    var parallax = $('.parallax');
+    var not_sticky=$("#not-sticky");
+    var me=$("#me");
+    var me_width = me.width();
 
-//     parallax.on('scroll', function () {
-//         if (parallax.scrollTop >= header.scrollTop) {
-//             header.addClass("sticky");
-//         } else {
-//             header.removeClass("sticky");
-//         }
-//     });
-// })
+    var offset = not_sticky.offset().top;
+
+    parallax.on('scroll', function () {
+        var current_offset = not_sticky.offset().top;
+        // console.log(current_offset-offset, me.width(), me_width)
+        if (current_offset <= 0) {
+            sticky.addClass("scrolled");
+        } else {
+            me.width(me_width+current_offset-offset);
+            sticky.removeClass("scrolled");
+        }
+    });
+})
