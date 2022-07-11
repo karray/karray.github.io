e92df73b081fa627acce1172abdddacf38bc8b9e
Aray Karjauv <kararray@gmail.com>
2022-07-12 00:03:48 +0200

    scrolling script

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,15 +8,15 @@ function getScroll() {
 let about = document.getElementById('about');
 let me = document.getElementById('me');
 let desc = document.getElementById('description');
-let vh5 = to_vh(5);
-let desc_height = to_vh(about.offsetHeight - desc.offsetHeight) - vh5
+
+let about_height = 60 - 15;
+let desc_height = to_vh(about.offsetHeight - desc.offsetHeight)
 let me_width = to_vh(me.offsetHeight);
 
-window.addEventListener('scroll', function () {
+function update_elements(){
     let scroll = getScroll()
     me.style.width = me_width  - scroll + 'vh';
-    console.log(getScroll(), to_vh(about.offsetHeight - desc.offsetHeight));
-    if(desc_height < getScroll()){
+    if(desc_height < scroll){
         desc.classList.add('scrolled')
         desc.classList.remove('fixed')
     }
@@ -24,4 +24,17 @@ window.addEventListener('scroll', function () {
         desc.classList.remove('scrolled')
         desc.classList.add('fixed')
     }
-});
+
+    if(scroll > about_height)
+        about.classList.add('hidden')
+    else
+        about.classList.remove('hidden')
+}
+
+window.addEventListener('scroll', update_elements);
+window.addEventListener('resize', function(event) {
+    desc_height = to_vh(about.offsetHeight - desc.offsetHeight)
+    update_elements()
+}, true);
+
+update_elements()
