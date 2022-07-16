4ac390dc4b61aacec406c02491355d557b06ca2b
Aray Karjauv <kararray@gmail.com>
2022-07-16 14:22:16 +0000

    fix header behaviour

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,52 +1,78 @@
-function to_vh(x){
-    return x / window.innerHeight*100;
+function getMinViewport(){
+    if(window.innerHeight > window.innerWidth){
+        return 'vw';
+    }
+    return 'vh';
 }
 function getScroll() {
-    return to_vh(window.pageYOffset || document.documentElement.scrollTop);
+    return getMinViewHeightWidth(window.pageYOffset || document.documentElement.scrollTop);
+}
+
+function getMinViewHeightWidth(x){
+    return x/Math.min(window.innerHeight, window.innerWidth)*100;
 }
 
 let about = document.getElementById('about');
 let me_img = document.getElementById('me');
 let desc = document.getElementById('description');
+let dummy_desc = document.getElementById('dummy-description');
+let info = document.getElementById('my-infobox');
+let unit = getMinViewport();
 
-let about_height = 60 - 15;
-let desc_height = to_vh(about.offsetHeight - desc.offsetHeight)
-let me_width = 50;
+let about_height = 60;
+// console.log(getMinViewHeightWidth(60))
+let desc_height = getMinViewHeightWidth(desc.offsetHeight)
+// let me_width = to_vh(me_img.offsetWidth);
 
-let is_small_screen = window.matchMedia( "(max-width: 1000px)" ).matches
+let is_small_screen = window.matchMedia('(max-width: 350px)')
 
 function update_elements(){
-    if(is_small_screen){
-        me_img.style.width = 'initial'
+    if(is_small_screen.matches){
+        about.classList.add('fixed-header')
+        // me_img.style.width = '100%'
         return
     }
 
     let scroll = getScroll()
-    me_img.style.width = me_width - scroll + 'vh';
-    if(desc_height < scroll){
-        desc.classList.add('scrolled')
-        desc.classList.remove('fixed')
+    let new_height = about_height - scroll
+    about.style.height = new_height + unit
+    if(desc_height > new_height){
+        if(!info.classList.contains('scrolled')){
+            dummy_desc.style.width = desc.offsetWidth+'px';
+            desc.style.width = desc.offsetWidth+'px';
+        }
+        info.classList.add('scrolled')
+        // desc.classList.remove('fixed')
     }
     else{
-        desc.classList.remove('scrolled')
-        desc.classList.add('fixed')
+        info.classList.remove('scrolled')
+        desc.style.width = 'auto';
+
+        // desc.classList.add('fixed')
     }
 
-    if(scroll > about_height)
+    if(new_height<15) {
         about.classList.add('fixed-header')
-    else
+        // me_img.style.width = '100%'
+    }
+    else {
         about.classList.remove('fixed-header')
-    
+        // me_img.style.width = me_width - scroll + 'vh';
+        // me_img.style.top = (60-me_width)/2 + scroll + 'vh';
+    }
 }
 
 window.addEventListener('scroll', update_elements);
 window.addEventListener('resize', function(event) {
-    desc_height = to_vh(about.offsetHeight - desc.offsetHeight)
-    is_small_screen = window.matchMedia( "(max-width: 1000px)" ).matches
+    // me_width = to_vh(me_img.offsetWidth);
+    // about_height = getMaxViewHeightWidth(60);
+    desc_height = getMinViewHeightWidth(desc.offsetHeight)
+    unit = getMinViewport();
+    // is_small_screen = window.matchMedia(`(max-width: ${breakpoint})`).matches
     update_elements()
 }, true);
 
-['#FFC0CB', '#DB7093', '#E6E6FA', '#D8BFD8', '#DDA0DD', '#BA55D3', '#7B68EE', '#663399', '#FFA07A', '#FA8072', '#FF8C00',
-'#FF6347', '#90EE90', '#3CB371', '#228B22', '	#9ACD32', '#66CDAA', '#20B2AA', '#008080', '#00CED1', '#6495ED']
+// ['#FFC0CB', '#DB7093', '#E6E6FA', '#D8BFD8', '#DDA0DD', '#BA55D3', '#7B68EE', '#663399', '#FFA07A', '#FA8072', '#FF8C00',
+// '#FF6347', '#90EE90', '#3CB371', '#228B22', '	#9ACD32', '#66CDAA', '#20B2AA', '#008080', '#00CED1', '#6495ED']
 
 update_elements()
